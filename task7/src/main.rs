52e100ee8890cfb26266324c0c0784f689ca639f
Softgod <softgod4@protonmail.com>
2024-02-06 18:36:27 +0300

    new tasks complete

--- task7/src/main.rs
+++ task7/src/main.rs
@@ -1,3 +1,15 @@
+fn is_pangram(s: &str) -> bool {
+    let mut vector_type: Vec<char> = Vec::new();
+    for c in s.chars() {
+        if  c.is_alphabetic() && !vector_type.contains(&c) {
+            vector_type.push(c);
+        }
+        return false;
+    }   
+    return true;
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("{}", is_pangram("This isn't a pangram!"));
+    println!("{}", is_pangram("The quick, brown fox jumps over the lazy dog!"));
 }
