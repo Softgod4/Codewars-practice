69b788bc185508016a607b5d582b984fb27a872a
Softgod <softgod4@protonmail.com>
2024-07-19 12:26:54 +0300

    Update 1.ts

--- task19/1.ts
+++ task19/1.ts
@@ -1,5 +1,6 @@
-export function josephusSurvivor(n: number, k: number) {
+export function change(s: string, prog: string, version: string): any {
     
 }
 
-console.log(josephusSurvivor(7, 3))
+console.log(change('Ladder', '1.1', 'Program: Ladder Author: g964 Phone: +1-503-555-0090 Date: 2019-01-01 Version: 1.1'))
+// 'Program title: Primes\nAuthor: Kern\nCorporation: Gold\nPhone: +1-503-555-0091\nDate: Tues April 9, 2005\nVersion: 6.7\nLevel: Alpha';
